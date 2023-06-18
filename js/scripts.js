5699a0ab206ca46f8e7c5d17d298e92ef34eaa7f
and <andersonvittor1801@gmail.com>
2023-06-18 12:27:20 -0300

    Terceiro commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,57 @@
+const f_tipoMilitar = document.querySelector('#f_tipoMilitar')
+const f_tipoNormal = document.querySelector('#f_tipoNormal')
+const f_blindagem = document.querySelector('#f_blindagem')
+const f_municao = document.querySelector('#f_municao')
+const carros = document.querySelector('#carros')
+const btn_addCarro = document.querySelector('#btn_addCarro')
+const f_nome = document.querySelector('#f_nome')
+const f_portas = document.querySelector('#f_portas')
+
+let a_carros = []
+
+f_tipoMilitar.addEventListener('click', (evt) => {
+    f_nome.value = ""
+    f_portas.value = 0
+    f_blindagem.value = 0
+    f_municao.value = 0
+    f_blindagem.removeAttribute("disabled")
+    f_municao.removeAttribute("disabled")
+})
+
+f_tipoNormal.addEventListener('click', (evt) => {
+    f_nome.value = ""
+    f_portas.value = 0
+    f_blindagem.value = 0
+    f_municao.value = 0
+    f_blindagem.setAttribute("disabled", "disabled")
+    f_municao.setAttribute("disabled", "disabled")
+})
+
+const gerenciarExibicaoCarros = () => {
+    carros.innerHTML = ""
+    a_carros.forEach((c) => {
+        const div = document.createElement('div')
+        div.setAttribute("class", "carro")
+        div.innerHTML = `Nome: ${c.nome}<br/>`
+        div.innerHTML += `Portas: ${c.portas}<br/>`
+        div.innerHTML += `Blindagem: ${c.blindagem}<br/>`
+        div.innerHTML += `Munição: ${c.municao}<br/>`
+        div.innerHTML += `Cor: ${c.cor}<br/>`
+        carros.appendChild(div)
+    })
+}
+
+btn_addCarro.addEventListener('click', () => {
+    if (f_tipoNormal.checked) {
+        const c = new Carro(f_nome.value, f_portas.value)
+        a_carros.push(c)
+    } else {
+        const c = new Militar(f_nome.value, f_portas.value, f_blindagem.value, f_municao.value)
+        a_carros.push(c)
+    }
+    gerenciarExibicaoCarros()
+})
+
 class Carro { // Classe PAI // Base
     constructor(nome, portas) {
         this.nome = nome
